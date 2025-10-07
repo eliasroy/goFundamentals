6f15f06fcbdffd3f4fd1960446c95f45aac90e6b
eliasroy <provinciaelias@gmail.com>
2025-10-06 22:24:34 -0500

    feat: variables and zero value

---
+++ src/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//constant
+	const PI = 3.14
+	fmt.Println("Value of PI:", PI)
+
+	//declaration of variable
+	var name string = "John Doe"
+	age := 30
+	var area int
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Area:", area)
+
+	//zero value
+	var a int
+	var b float64
+	var c string
+	var d bool
+	c = "hello everyone"
+	fmt.Println("Zero value of int:", a)
+	fmt.Println("Zero value of float64:", b)
+	fmt.Println("Zero value of string:", c)
+	fmt.Println("Zero value of bool:", d)
+
+}
